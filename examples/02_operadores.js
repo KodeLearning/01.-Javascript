9c073f30e19c11718cfd4e4516a8644cd1b3c9a9
Gotti <143560348+Inmortia@users.noreply.github.com>
2023-12-04 22:59:55 +0100

    operators added

---
+++ examples/02_operadores.js
@@ -0,0 +1,55 @@
+// Asignación
+let x = 10;
+let y = 5;
+
+// Aritméticos
+console.log(x + y);
+
+const suma = x + y;
+
+console.log(suma);
+
+const diferencia = suma / 2;
+
+const producto = x * y;
+
+console.log(diferencia);
+
+const modulo = 4 % 2;
+
+console.log("Modulo: " + modulo);
+
+console.log("12" - "1"); // wat
+
+console.log("12D" - 1);
+
+// x = x + 50
+x += 50;
+x++; // x = x + 1
+console.log(x); /// 61
+
+// y = y - 2
+y -= 2;
+y--; // y = y - 1
+console.log(y); /// 2
+
+// -- Operadores de comparación
+console.log("<------ Operadores de comparación ------>");
+console.log("Hola" === "Hola");
+
+const num = "12";
+console.log(num === 12);
+
+console.log("Hola" !== "Hola");
+
+let a = 5,
+  b = 8;
+
+console.log(a < b);
+console.log(a < b);
+console.log(a <= b);
+console.log(a <= b);
+
+console.log(NaN === NaN); // wat
+
+console.log(0 == false);
