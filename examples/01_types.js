a9f9bb80af9b7bc231cff56e1a921834ee83016c
Gotti <143560348+Inmortia@users.noreply.github.com>
2023-12-04 20:51:11 +0100

    javascript types

---
+++ examples/01_types.js
@@ -0,0 +1,14 @@
+let precio = 199.99;
+
+let producto = 'Zapatillas "Air Jordan"';
+producto = 'Zapatillas "Air Jordan"';
+
+let stock = Infinity;
+
+stock = 0;
+
+stock = null;
+
+stock = undefined;
+
+console.log(typeof stock);
