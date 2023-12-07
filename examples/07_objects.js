c255448fa60837353b732ea6b396c8e552b9cfbe
Gotti <143560348+Inmortia@users.noreply.github.com>
2023-12-07 23:00:35 +0100

    Objects theory

---
+++ examples/07_objects.js
@@ -0,0 +1,26 @@
+console.log('Objects')
+
+// const product = new Object()
+const product = {
+  name: 'Air Jordan',
+  price: 123,
+  stock: null,
+}
+
+const fordMustang = {
+  brand: 'Ford',
+  year: 1969,
+  model: 'Mustang',
+  firstOwner: 'Inmortia',
+  stock: null,
+  country: {
+    name: 'USA',
+    code: 'us',
+  },
+  owners: ['Inmortia', 'Elon', 'Jordan'],
+  'second-owner': 'Elon',
+}
+
+const cars = [fordMustang]
+
+console.log(cars)
