c437a2f1f1a77bad405aaf6d5872e140e2c9e91f
Gotti <143560348+Inmortia@users.noreply.github.com>
2023-12-07 22:28:08 +0100

    Ejercicios WHILE

---
+++ exercises/while_loop.js
@@ -0,0 +1,31 @@
+/**
+ * Piedra papel tijera
+ * Vamos a preguntar al usuario su jugada, que tiene que ser "piedra", "papel" o "tijera"
+ *
+ * - Si no es ninguna mostrar un mensaje de "opción incorrecta"
+ * - Si el usuario quiere salir que escriba "salir"
+ * - El rival siempre va a tener piedra
+ * - Implementar la lógica
+ *   - "Ganaste tu tienes papel y el rival piedra"
+ *   - "Perdiste tu tienes tijeras y el rival piedra"
+ *   - "Empate ambos tienen piedra"
+ **/
+
+while (true) {
+  let choice = prompt('¿Piedra, papel o tijera?')
+  choice = choice.toLowerCase()
+
+  console.clear()
+  if (choice === 'piedra') {
+    console.log('CPU sacó piedra!\n¡Empate!')
+  } else if (choice === 'papel') {
+    console.log('CPU sacó piedra!\n¡Has ganado!')
+  } else if (choice === 'tijera') {
+    console.log('CPU sacó piedra\n¡Has perdido!')
+  } else if (choice === 'salir') {
+    console.log('Ciao :(')
+    break
+  } else {
+    console.log('Opción incorrecta')
+  }
+}
