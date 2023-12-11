7e957f9ce02c0d7332fd2d5b20f9a1860019a770
Gotti <143560348+Inmortia@users.noreply.github.com>
2023-12-11 19:54:00 +0100

    show object info and render

--- examples/07_objects.js
+++ examples/07_objects.js
@@ -54,4 +54,31 @@ for (let index = 0; index < removeKeys.length; index++) {
   delete car[keyToRemove]
 }
 
-console.log(car)
+// Info
+const shows = [
+  {
+    id: 1,
+    name: 'Under the dome',
+    genres: ['Drama', 'Science-Fiction', 'Thriller'],
+  },
+  {
+    id: 2,
+    name: 'Game of thrones',
+    genres: ['Drama', 'Thriller'],
+  },
+]
+
+let showTemplate = ''
+for (let i = 0; i < shows.length; i++) {
+  const show = shows[i]
+  // Mostrar
+  showTemplate =
+    showTemplate +
+    `
+    <h2>${show.name}</h2>
+    <p>${show.genres.join(', ')}</p>
+  `
+}
+
+// Renderizar
+document.write(showTemplate)
