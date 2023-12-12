6d03056bc668bfe2e79695ab05e118629e4e5fd9
Gotti <143560348+Inmortia@users.noreply.github.com>
2023-12-12 20:51:08 +0100

    ejercicio de funciones

---
+++ exercises/function.js
@@ -0,0 +1,54 @@
+/**
+ *
+ * Enunciado: Dada una lista de números, crea dos nuevas listas,
+ * una que contenga solo los números impares de la lista original
+ * y la otra los pares. Cuidado que la lista puede no contener números.
+ *
+ * Lista de números: 1, 2, 33, 41, 5, 60, 74, 87, 90, 101, null, '', false
+ *
+ **/
+
+const numsData = [1, 2, 33, 41, 5, 60, 74, 87, 90, 101, null, '', false]
+const NumTypes = {
+  Even: 'even',
+  Odd: 'odd',
+  Both: 'both',
+}
+
+const evenOrOdd = (numList, numType = 'even') => {
+  const evenNums = []
+  const oddNums = []
+  let nums = {}
+
+  for (let i = 0; i < numList.length; i++) {
+    const num = numList[i]
+    if (typeof num === 'number') {
+      if (
+        (numType === NumTypes.Even || numType === NumTypes.Both) &&
+        num % 2 === 0
+      ) {
+        evenNums.push(num)
+      } else if (
+        (numType === NumTypes.Odd || numType === NumTypes.Both) &&
+        num % 2 > 0
+      ) {
+        oddNums.push(num)
+      }
+    }
+  }
+
+  if (numType === NumTypes.Even) {
+    nums = { evens: evenNums }
+  } else if (numType === NumTypes.Odd) {
+    nums = { odds: oddNums }
+  } else if (numType === NumTypes.Both) {
+    nums = {
+      evens: evenNums,
+      odds: oddNums,
+    }
+  }
+
+  return nums
+}
+
+console.log(evenOrOdd(numsData, 'both'))
