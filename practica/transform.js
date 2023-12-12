4b2dc6768935d54bde33907494fcf4abe4f6873c
Gotti <143560348+Inmortia@users.noreply.github.com>
2023-12-12 13:57:58 +0100

    practica: ejercicio 3

---
+++ practica/transform.js
@@ -0,0 +1,64 @@
+/**
+ * const desarrolladoresJavascript = [
+ *  {
+ *    "id": 1,
+ *    "nombre": "Juan",
+ *    "habilidades": ["JavaScript", "HTML", "CSS"],
+ *    "proyectos": [
+ *      { "id": 1, "nombre": "Proyecto 1"},
+ *      { "id": 2, "nombre": "Proyecto 2" }
+ *    ]
+ *  }
+ * ]
+ *
+ * const nombresProyectos = ['Proyecto 1', 'Proyecto 2', 'Proyecto 3',
+ * 'Proyecto 4', 'Proyecto 5', 'Proyecto 6']
+ **/
+
+const datos = [
+  {
+    id: 1,
+    nombre: 'Juan',
+    habilidades: ['JavaScript', 'HTML', 'CSS'],
+    proyectos: [
+      { id: 1, nombre: 'Proyecto 1' },
+      { id: 2, nombre: 'Proyecto 2' },
+    ],
+  },
+  {
+    id: 2,
+    nombre: 'María',
+    habilidades: ['Python', 'SQL', 'Django'],
+    proyectos: [
+      { id: 3, nombre: 'Proyecto 3' },
+      { id: 4, nombre: 'Proyecto 4' },
+    ],
+  },
+  {
+    id: 3,
+    nombre: 'Pedro',
+    habilidades: ['Java', 'Spring', 'Hibernate'],
+    proyectos: [
+      { id: 5, nombre: 'Proyecto 5' },
+      { id: 6, nombre: 'Proyecto 6' },
+    ],
+  },
+]
+
+const desarrolladoresJavascript = []
+const nombresProyectos = []
+for (const worker of datos) {
+  let i = 0
+  while (i < worker.habilidades.length) {
+    if (worker.habilidades[i] === 'JavaScript') {
+      desarrolladoresJavascript.push(worker)
+    }
+    i++
+  }
+
+  for (const project of worker.proyectos) {
+    nombresProyectos.push(project.nombre)
+  }
+}
+console.log('Desarrolladores de Javascript:', desarrolladoresJavascript)
+console.log('Proyectos activos:', nombresProyectos)
