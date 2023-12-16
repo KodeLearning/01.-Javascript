b62ffb9c56656a8c40a80dcefa734bcbf213d76b
Gotti <143560348+Inmortia@users.noreply.github.com>
2023-12-16 17:15:38 +0100

    createMatch() done

--- practica/wimblecode-sim/index.js
+++ practica/wimblecode-sim/index.js
@@ -1,8 +1,30 @@
 const wimblecode = () => {
+  let players = {}
+  let winners = {}
+
+  const createMatch = (player1, player2) => {
+    players = {
+      g1: {
+        player1: { name: player1, points: 0 },
+        player2: { name: player2, points: 0 },
+      },
+    }
+    console.log(players)
+    return players
+  }
+
+  const pointWonBy = (playerId) => {}
+  const getCurrentRoundScore = () => {}
+  const getRoundsScore = () => {}
+
   return {
-    createMatch,
-    pointWonBy,
-    getCurrentRoundScore,
-    getRondScore,
+    createMatch, // Inicializa dos jugadores que van a competir
+    pointWonBy, // Otorga 1 punto a uno de los jugadores
+    getCurrentRoundScore, // Muestra los puntos actuales de la ronda iniciada
+    getRoundsScore, // Muestra el total de juegos ganados por los dos jugadores
   }
 }
+
+const game = wimblecode()
+
+console.log(game.createMatch('David', 'Ali'))
